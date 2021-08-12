0df328b7bfc6a838de7b9ffa197722d54724f3f3
unknown <diecristher@gmail.com>
2021-08-12 18:03:34 +0200

    Feat: add and test some features

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render, HttpResponse
 
+html_base = """
+<h1> Mi web personal</h1>
+<u1>
+    <li><a href="/">Portada</a></li>
+    <li><a href="/about-me/">Acerca de</a></li>
+</u1>
+"""
 def home(request):
-    return HttpResponse("<h1>Mi web personal</h1><h2>Portada</2>")
+    return HttpResponse(html_base + """
+    <h1>Mi web personal</h1>
+    <h2>Portada</2>
+    """)
+
+def about(request):
+    return HttpResponse(html_base + """
+    <h2>Acerca de</2>
+    <p>Soy Diego y soy programador</p>
+    """)
 
 # Create your views here.
