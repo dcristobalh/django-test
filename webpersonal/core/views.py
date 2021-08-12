50b7762562248b4056c9271db95901eea4a22b02
unknown <diecristher@gmail.com>
2021-08-12 19:31:52 +0200

    Feat: add templates folder and clean views.py

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,34 +1,14 @@
 from django.shortcuts import render, HttpResponse
 
-html_base = """
-<h1> Mi web personal</h1>
-<u1>
-    <li><a href="/">Portada</a></li>
-    <li><a href="/about-me/">Acerca de</a></li>
-    <li><a href="/portfolio/">Portfolio</a></li>
-    <li><a href="/contact/">Contacto</a></li>
-</u1>
-"""
 def home(request):
-    return HttpResponse(html_base + """
-    <h1>Mi web personal</h1>
-    <h2>Portada</2>
-    """)
+    return render(request, "core/home.html")
 
 def about(request):
-    return HttpResponse(html_base + """
-    <h2>Acerca de</2>
-    <p>Soy Diego y soy programador</p>
-    """)
+    return render(request, "core/acerca_de.html")
 
 def portfolio(request):
-    return HttpResponse(html_base + """
-    <h2>Portfolio</2>
-    <p>Hola esto es un portfolio</p>
-    """)
+    return render(request, "core/portfolio.html")
+
 def contact(request):
-    return HttpResponse(html_base + """
-    <h2>Contact</2>
-    <p>Este es mi contacto</p>
-    """)
+    return render(request, "core/contact.html")
 # Create your views here.
